42f3e1457d22e5cd4a8d866246bf2e2effca2bc2
changchuntzu0618 <chun-tzu.chang@epfl.ch>
2024-05-07 10:58:51 +0200

    fix get visual

--- model/visual_embedding/get_visual_embedding.py
+++ model/visual_embedding/get_visual_embedding.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 import cv2
 from visual_embeding_detectron2 import VisualEmbedder
-import tqdm
+from tqdm import tqdm
 
 
 train_data_path='../../data/hateful_memes/train_df_wQuery_.jsonl'
@@ -56,7 +56,7 @@ visualembedder=VisualEmbedder(cfg_path=cfg_path, min_boxes=MIN_BOXES, max_boxes=
 
 img_data = pd.read_json(path_or_buf=data_path, lines=True).to_dict(orient='records')
 
-for i, item in tqdm(enumerate(img_data)):
+for i, item in enumerate(tqdm(img_data)):
     image_list = [cv2.imread(os.path.join(img_dir, item['img'].split('/')[-1]))]
     visual_embeds = visualembedder.visual_embeds_detectron2(image_list)
     img_data[i]['visual_embedding'] = visual_embeds
