009b2fb635b675cac0a36cc7ff5ea7dd7c39709a
changchuntzu0618 <chun-tzu.chang@epfl.ch>
2024-05-22 10:55:42 +0200

    update model

--- model/model.py
+++ model/model.py
@@ -131,56 +131,43 @@ class HateMemeClassifier(torch.nn.Module):
         visual_embedder: 'vit' or 'detectron2'
         """
         super(HateMemeClassifier, self).__init__()
-        self.fusion_method = fusion_method # 'concatenate' or 'weight_ensemble' or 'linear_weight_ensemble'
+        self.fusion_method = fusion_method # 'concatenate' or 'weight_ensemble' or 'linear_weight_ensemble' or 'visualbert'
         self.wandb_run=wandb_run
 
         configuration = VisualBertConfig.from_pretrained('uclanlp/visualbert-nlvr2-coco-pre',
                                                 hidden_dropout_prob=0.1, attention_probs_dropout_prob=0.1)
         self.visualbert = VisualBertModel.from_pretrained('uclanlp/visualbert-nlvr2-coco-pre', config=configuration)
-        self.bertmodel = BertModel.from_pretrained('bert-base-uncased')
-        for param in self.visualbert.parameters():
-            param.requires_grad = False
-        for param in self.bertmodel.parameters():
-            param.requires_grad = False
+        if self.fusion_method != 'visualbert':
+            self.bertmodel = BertModel.from_pretrained('bert-base-uncased')
 
         if visual_embedder=='vit':
             self.embed_cls = nn.Linear(768, 1024)
         elif visual_embedder=='detectron2':
             self.embed_cls = nn.Linear(1024, 1024)
         self.num_labels = 2
-        self.dropout = nn.Dropout(0.1)
-        
+        self.dropout = nn.Dropout(0.3)
     
         if self.fusion_method=='weight_ensemble':
             self.alpha = nn.Parameter(torch.tensor(0.5))  # Initial value of alpha
-            self.cls= nn.Sequential(
-                nn.Linear(768, 768),
-                nn.ReLU(),
-                nn.Dropout(0.1),
-                nn.Linear(768, 768),
-                nn.ReLU(),
-                nn.Dropout(0.1),
-                nn.Linear(768, 768),
-                nn.ReLU(),
-                nn.Dropout(0.1),
-                nn.Linear(768, 768),
-                nn.ReLU(),
-                nn.Dropout(0.1),
-                nn.Linear(768, 768),
-                nn.ReLU(),
-                nn.Dropout(0.1),
-                nn.Linear(768, 348),
-                nn.ReLU(),
-                nn.Dropout(0.1),
-                nn.Linear(348, 192),
-                nn.ReLU(),
-                nn.Linear(192, self.num_labels)
-            )
-        # TODO: Calculate the weights for the loss function and weight balanced loss
-        # nSamples = [5178, 2849]
-        # normedWeights = [1 - (x / sum(nSamples)) for x in nSamples]
-        # self.loss_fct = CrossEntropyLoss(weight=torch.FloatTensor(normedWeights))
-        self.loss_fct = CrossEntropyLoss()
+
+        self.cls= nn.Sequential(
+            nn.Linear(768, 348),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.BatchNorm1d(348),
+            nn.Linear(348, 192),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.BatchNorm1d(192),
+            nn.Linear(192, self.num_labels)
+        )
+
+        # Calculate the weights for the loss function and weight balanced loss
+        nSamples = [5450,3050]
+        normedWeights = [1 - (x / sum(nSamples)) for x in nSamples]
+        self.loss_fct = CrossEntropyLoss(weight=torch.FloatTensor(normedWeights))
+
+        # self.loss_fct = CrossEntropyLoss()
         
     
     def forward(self, input_ids, attention_mask, token_type_ids, visual_embeds, visual_attention_mask,
@@ -191,32 +178,35 @@ class HateMemeClassifier(torch.nn.Module):
         well as arbitrary operators on Tensors.
         """
         visual_embeds_cls = self.embed_cls(visual_embeds)
-        with torch.no_grad():
-            outputs = self.visualbert(
-                    input_ids,
-                    attention_mask=attention_mask,
-                    token_type_ids=token_type_ids,
-                    visual_embeds=visual_embeds_cls,
-                    visual_attention_mask=visual_attention_mask,
-                    visual_token_type_ids=visual_token_type_ids,
-                )
+        
+        outputs = self.visualbert(
+                input_ids,
+                attention_mask=attention_mask,
+                token_type_ids=token_type_ids,
+                visual_embeds=visual_embeds_cls,
+                visual_attention_mask=visual_attention_mask,
+                visual_token_type_ids=visual_token_type_ids,
+            )
         
         visualbert_embedding = outputs[1] # output is a context vector of 768 dimensions
 
+        if self.fusion_method != "visualbert":
         
-        with torch.no_grad():
             caption_outputs = self.bertmodel(caption_input_ids, attention_mask=caption_attention_mask, token_type_ids=caption_token_type_ids)
-                
-        # Get the embeddings of the [CLS] token
-        caption_embeddings = caption_outputs.last_hidden_state[:,0,:] # output is a context vector of 768 dimensions
+                    
+            # Get the embeddings of the [CLS] token
+            caption_embeddings = caption_outputs.last_hidden_state[:,0,:] # output is a context vector of 768 dimensions
         
         if self.fusion_method=='weight_ensemble':
             # funsion model: weight ensenble of the two embeddings: alpha*visualbert_embedding + (1-alpha)*caption_embeddings 
             fused_embedding = self.alpha * self.dropout(visualbert_embedding) + (1-self.alpha) * self.dropout(caption_embeddings)
             self.wandb_run.log({"alpha": self.alpha.data.cpu().numpy()},commit=False)
         
-        logits = self.cls(fused_embedding)
-        ##
+            logits = self.cls(fused_embedding)
+        
+        if self.fusion_method=='visualbert':
+            logits = self.cls(self.dropout(visualbert_embedding))
+        
         
         reshaped_logits = logits.view(-1, self.num_labels)
         loss = self.loss_fct(reshaped_logits, labels.view(-1))
